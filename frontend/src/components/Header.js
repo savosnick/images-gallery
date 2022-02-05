46c65f960b2117bf494489a9d6db06fd236f91ae
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-02-05 14:40:01 -0800

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: '12rem', maxHeight: '3rem' }} />
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '3rem' }} />
       </Container>
     </Navbar>
   );
