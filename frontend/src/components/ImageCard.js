056f813612f1de2cf08a3c62e8d479464f0005ba
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-02-05 11:56:09 -0800

    Display and Delete Images

---
+++ frontend/src/components/ImageCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+const ImageCard = ({ image, DeleteImage }) => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>
+          {image.title[0].toUpperCase() + image.title.slice(1)}
+        </Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => DeleteImage(image.id)}>
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ImageCard;
