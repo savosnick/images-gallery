ea5738040f94b5cb7b109f15d074490ab98a494a
Jonathan Savosnick <jonathan.savosnick@gmail.com>
2022-02-24 20:42:52 -0800

    Removing VS Container

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" disabled={!word} type="submit">
-                  Search Images Today
+                  Search Images
                 </Button>
               </Col>
             </Form.Row>
